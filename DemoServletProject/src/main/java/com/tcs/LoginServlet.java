d1b5902e05c633d7a4ba5d0539398a25ca169c2c
mysurareddy <reddymysura92@gmail.com>
2023-05-26 00:47:09 +0530

    java file edited

--- DemoServletProject/src/main/java/com/tcs/LoginServlet.java
+++ DemoServletProject/src/main/java/com/tcs/LoginServlet.java
@@ -19,6 +19,8 @@ public class LoginServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
+		response.setContentType("text/html");
+		response.setContentType("text/html");
 		
 	}
 
