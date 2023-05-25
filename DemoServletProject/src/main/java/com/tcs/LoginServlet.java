f84a7b5493b295893d4290cb44d609d333a2e135
mysurareddy <reddymysura92@gmail.com>
2023-05-25 23:44:46 +0530

    First commit

---
+++ DemoServletProject/src/main/java/com/tcs/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.tcs;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
